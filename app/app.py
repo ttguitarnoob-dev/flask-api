214696046ccccebfdcd3f8ecfa60e713eced10b9
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-05-24 13:49:21 -0500

    removed the commented stuff that I ended up retyping anyway

--- app/app.py
+++ app/app.py
@@ -1,18 +1,3 @@
-# from flask import Flask, jsonify, request
-# import logging as logger
-# from api_things import *
-
-
-# logger.basicConfig(level="DEBUG")
-
-# app = Flask(__name__)
-
-# if __name__ == '__main__':
-#     logger.debug("Starting the Application and it smells")
-    
-#     app.run(host="0.0.0.0", port=5000, debug = True)
-
-
 from flask import Flask, request
 from flask_restful import Resource, Api
 import logging as logger
